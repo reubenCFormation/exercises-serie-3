14a478874d7bb797b4b729c1a6f71f7e4109cfe9
yunities-front <reubenCFormation@gmail.com>
2023-05-05 11:40:34 +0200

    exo-js-serie3

---
+++ ex2.js
@@ -0,0 +1,10 @@
+/*
+Utilisez les données fourni ainsi que le fichier ex2.html
+1) Ciblez la classe "concerts-title". Rajoutez de maniere dynamique, le nombre actuelle de concerts. Sans compter le tableau a la main!
+2) Pour chaque concert, creez une balise h3 pour afficher le titre du concert, une balise h4 pour afficher  la ville du concert, 
+3) Creez une ul pour lister tous les groupes qui joue pour le concert en cours. Apres chaque concert rajoutez un br pour espacer un peu
+
+*/
+
+var concerts=[{title:"The best concert",city:"San Francisco",bands:["Orelsan","Booba","Maitre gims","Kid Cudi"]},{title:"Le concert a paris",city:"Paris",bands:["Orelsan","Maitre Gims","Sexion D'assuat","N.T.M","Iam"]},{title:"Le concert au sud",city:"Nice",bands:["Johnny Halliday","Booba","Vald","Casseurs Flowthers"]},{title:"The big apple",city:"New York",bands:["The Rolling Stones","The Beatles","Green Day","Hollywood Undead"]}];
+
---
+++ ex3.js
@@ -0,0 +1,14 @@
+
+/*
+    BONUS!, pour ceux qui se sente a l'aise avec les deux exo precedantes! 
+      Selon les données fournis dans le tableau restaurants
+   1)Utilisez le tableau html fourni dans le fichier ex3.html 
+   2) Construisez le tableau html pour afficher le nom du restaurant, le quartier dans lequelle se trouve le restaruant, et la liste des plats du restaurant
+   3) Si le restaurant est vegetarian, les plats seront afficher en vert. Sinon, les plats seront afficher en orange
+   4) Bonus:ecrivez votre code en 2 fonctions different!
+
+    A noter: Cette exercise n'est pas facile, mais a une logique assez similaire que notre dynamicList.js, si vous suiviez la logique de cette exercise, vous devriez pouvoir vous retrouver!
+*/
+
+
+var restaurants=[{name:"Katz deli",neighborhood:"East Village",isVegetarian:false,meals:["Pastrami Sandwich","Corned beef Sandwich","Matza Ball Soup","Cheesecake","Cheesefries","BLT"]},{name:"Krusty Pizza",neighborhood:"Dyker Heights",isVegetarian:false,meals:["Triple crown pizza","Pepperoni Pizza","The hot melt","Maragarita Pizza","Meat Lovers","Italian cheese balls","Veggie Lovers"]},{name:"Plant Lovers",neighborhood:"West Village",isVegetarian:true,meals:["grass juice","broccoli","tofu burder"]},{name:"Vegan Corner",neighborhood:"Williamsburgh",isVegetarian:true,meals:["Tofu Burger","vegan steak","vegan cheesey"]},{name:"Sunday In Brooklyn",neighborhood:"Williamsburgh",isVegetarian:false,meals:["Choco Waffle","Breakfast burrito","Crispy Chicken Burger","Signature Burder","Foie Gras","The Sunday Pizza"]},{name:"The Lighthouse Cafe",neighborhood:"Bayonne",isVegetarian:false,meals:["Philly Cheesesteak","tripe dipped pancakes"]}];
